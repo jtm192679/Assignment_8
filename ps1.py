028b98f793a3a2e06bce5f562611c18791afc5db
jtm192679 <jtm192679@iit.ac.in>
2019-09-18 10:23:33 +0530

    printed binary data with parity check

--- ps1.py
+++ ps1.py
@@ -1,1 +1,17 @@
+bin1 = input("enter binary data:")        # Driver code
+a = list(bin1)
+print (a)
+b = a.count("1")
+print (b)
+if(b%2==0):
+    a.append(1)
+
+else:
+    a.append(0)
+print (a)
+
+
+
+
+
 

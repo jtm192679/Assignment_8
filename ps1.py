cc084b078677ceccb1d8900aa34b08bf050ece78
jtm192679 <jtm192679@iit.ac.in>
2019-09-18 14:50:08 +0530

    corrected some error and write comments

--- ps1.py
+++ ps1.py
@@ -1,36 +1,26 @@
 def printIndex(str, s):
     flag = False;
     for i in range(len(str)):
-        if (str[i:i + len(s)] == s):
+        if (str[i:i + len(s)] == s):             #taking index of list
             print(i, end=" ");
             flag = True;
             x = list(str)
-            x.insert((i+3),"0")
+            x.insert((i + 3), "0")
+            print(x)
 
 
-        if (flag == False):
-            #print("NONE");
-        print(x)
-
-bin1 = input("enter binary data:")        # Driver code
-a = list(bin1)
-print (a)
-b = a.count("1")
-print (b)
-if(b%2==0):
+bin1 = input("enter binary data:")  # Driver code taking input binary data as string
+a = list(bin1)                    #converting string into list
+print(a)
+b = a.count("1")                  #counting for number of ones
+print(b)
+if (b % 2 == 0):                  #checking for odd and even parity
     a.append(1)
 
 else:
     a.append(0)
-print (a)
+print(a)
 bin2 = "010"
-printIndex(bin1,bin2)
-p= a.append("0101")
-print (p)
-
-
-
-
-
-
-
+printIndex(bin1, bin2)               #stuffing 0 after each 010
+p = a.append("0101")
+print(p)

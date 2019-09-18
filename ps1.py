b497d27a772241e3ccc826fb41d6591acfac2f75
jtm192679 <jtm192679@iit.ac.in>
2019-09-18 11:31:46 +0530

    insert 0 after appearnce of 010

--- ps1.py
+++ ps1.py
@@ -1,3 +1,17 @@
+def printIndex(str, s):
+    flag = False;
+    for i in range(len(str)):
+        if (str[i:i + len(s)] == s):
+            print(i, end=" ");
+            flag = True;
+            x = list(str)
+            x.insert((i+3),"0")
+
+
+        if (flag == False):
+            #print("NONE");
+        print(x)
+
 bin1 = input("enter binary data:")        # Driver code
 a = list(bin1)
 print (a)
@@ -9,6 +23,11 @@ if(b%2==0):
 else:
     a.append(0)
 print (a)
+bin2 = "010"
+printIndex(bin1,bin2)
+p= a.append("0101")
+print (p)
+
 
 
 
